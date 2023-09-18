7af255525936addb1df001a674114ab8a0ed2638
thejjwang <jonathanjacobwang@gmail.com>
2023-09-18 12:21:30 -0700

    mind grenade

--- 3-modules.js
+++ 3-modules.js
@@ -2,8 +2,8 @@
 // Modules - Encapsulated Code (only share minimum)
 const names = require('./04-names')
 const sayHi = require('./05-utils')
-// const data = require('./06-alternative-flavor')
-// require('./07-mind-grenade')
+const data = require('./06-alternative-flavor')
+require('./07-mind-grenade')
 
 sayHi('susan')
 sayHi(names.john)
